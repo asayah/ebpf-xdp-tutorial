6695828120d16694fd82c0974553cd9a9fe1ce5f
asayah <adam@solo.io>
2022-11-27 17:22:04 -0700

    exec

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker run -d --rm --name target-A -h target-A --env TERM=xterm-color nginxdemos
 docker run -d --rm --name target-B -h target-B --env TERM=xterm-color nginxdemos/hello:plain-text
 docker run -d -t -h client --name client --env TERM=xterm-color base
 docker run -d -t -h restricted-client --name restricted-client --env TERM=xterm-color base
-docker run -d --privileged -h router --name router --env TERM=xterm-color base
+docker run -v $(pwd):/tutorial -d --privileged -h router --name router --env TERM=xterm-color base
